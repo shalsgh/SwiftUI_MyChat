6056b3c5e55833b575dbb559b1552deedd926bf3
swtapple <hihibye3000@gmail.com>
2023-11-22 23:42:13 +0900

    기본코드

---
+++ MyChat/MyChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyChatApp.swift
+//  MyChat
+//
+//  Created by minho on 2023/11/22.
+//
+
+import SwiftUI
+
+@main
+struct MyChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
